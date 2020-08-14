ec4862d3d467533f3593c70d67509804a4abc9b2
karin <doykarin@gmail.com>
2020-08-14 13:21:21 -0300

    colocando imagem de gato

--- index.js
+++ index.js
@@ -14,24 +14,46 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
+  console.log("get /");
   res.send({ works: `Eu estou funcionando!!` })
+
+  console.log("\n");
 });
 
 app.post('/cadastro', (req, res) => {
-  const { name } = request.body;
+  console.log("post /cadastro");
+  const { name } = req.body;
+  console.log(req.body);
+  console.log(name)
   res.send({ welcome: `Eai ${name}, ja fiz seu cadastro.` })
+
+  console.log("\n");
 });
 
 app.get('/cadastro/:id', (req, res) => {
+  console.log("get /cadastro/:id");
   const { id } = req.params;
+  console.log("id: "+id);
+
   res.send({ greetings: `Hello ${id}!!` })
+
+  console.log("\n");
+});
+
+app.get('/gato/', (req, res) => {
+  console.log("get /gato/:id");
+  res.send({ gato: `Miauuuuu` })
 });
 
 app.get('/gato/:id', (req, res) => {
-  res.send({ gato: `Miauuuuu` })
+  console.log("get /gato/:id");
+  res.sendFile(__dirname + '/src/img/100.jpeg')
 });
+
+
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+
 })
